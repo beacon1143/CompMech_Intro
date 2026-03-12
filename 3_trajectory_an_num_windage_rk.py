88ac93e6785692cf43726f0a3d2f187c98bf1819
beacon1143 <beacon1143@gmail.com>
2026-03-12 18:02:17 +0300

    traject with windage: Euler and Runge-Kutta methods

--- 3_trajectory_an_num_windage_rk.py
+++ 3_trajectory_an_num_windage_rk.py
@@ -31,18 +31,49 @@ ax.axis('equal')
 traject_an = ax.plot(x_an, y_an, lw=3)[0]
 traject_num = ax.plot(x_num[0], y_num[0], lw=3)[0]
 
+def derivatives(x, y, vx, vy):
+    dxdt = vx
+    dydt = vy
+    dvxdt = -windage * vx
+    dvydt = -g - windage * vy
+    return dxdt, dydt, dvxdt, dvydt
+
+
+def rk4_step(x, y, vx, vy, dt):
+    k1x, k1y, k1vx, k1vy = derivatives(x, y, vx, vy)
+    k2x, k2y, k2vx, k2vy = derivatives(
+        x + 0.5*dt*k1x,   y + 0.5*dt*k1y,
+        vx + 0.5*dt*k1vx, vy + 0.5*dt*k1vy
+    )
+    k3x, k3y, k3vx, k3vy = derivatives(
+        x + 0.5*dt*k2x,   y + 0.5*dt*k2y,
+        vx + 0.5*dt*k2vx, vy + 0.5*dt*k2vy
+    )
+    k4x, k4y, k4vx, k4vy = derivatives(
+        x + dt*k3x,   y + dt*k3y,
+        vx + dt*k3vx, vy + dt*k3vy
+    )
+    x += dt*(k1x + 2*k2x + 2*k3x + k4x)/6
+    y += dt*(k1y + 2*k2y + 2*k3y + k4y)/6
+    vx += dt*(k1vx + 2*k2vx + 2*k3vx + k4vx)/6
+    vy += dt*(k1vy + 2*k2vy + 2*k3vy + k4vy)/6
+    return x, y, vx, vy
+
+
 def init_anim():
     traject_an.set_data(x_an, y_an)
     traject_num.set_data(x_num[:1], y_num[:1])
     return (traject_an, traject_num)
 
+
 def loop_anim(i):
     global vx, vy
-    x_num[i + 1] = x_num[i] + vx * dt
-    vx = vx * (1.0 - windage * dt)
-    y_num[i + 1] = y_num[i] + vy * dt
-    vy = vy - (g + windage * vy) * dt
-    if y_num[i + 1] <= 0:
+
+    x_num[i+1], y_num[i+1], vx, vy = rk4_step(
+        x_num[i], y_num[i],
+        vx, vy, dt
+    )
+    if y_num[i+1] <= 0:
         return (traject_an, traject_num)
     traject_num.set_data(x_num[:i+2], y_num[:i+2])
     return (traject_an, traject_num)
